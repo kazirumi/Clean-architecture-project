4ffc6f2b807cb1f1110358c1b1e1e0afba5deba7
kazirumi <tangorumi@gmail.com>
2025-06-15 18:15:33 +0600

    Dependency Injections process added

--- Project.Api/Program.cs
+++ Project.Api/Program.cs
@@ -1,9 +1,14 @@
+using Project.Application;
+using Project.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication().AddInfrastructure();
 
 var app = builder.Build();
 
